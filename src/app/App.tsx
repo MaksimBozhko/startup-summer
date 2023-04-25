4586b0a752ad116652f63040e48b0c1b6a4ca170
MaksimBozhko <bochkomax2000@gmail.com>
2023-04-26 02:03:41 +0300

    delete: login

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,7 +6,6 @@ import { Header } from "../components/header"
 import { ROUTES } from "../constant"
 import { Search } from "../pages/search"
 import { Vacancy } from "../pages/vacancy"
-import { Login } from "../features/login"
 
 function App() {
   return (
@@ -16,7 +15,6 @@ function App() {
         <Routes>
           <Route path={ROUTES.SEARCH_PAGE} element={<Search />} />
           <Route path={ROUTES.VACANCY_PAGE} element={<Vacancy />} />
-          <Route path={ROUTES.LOGIN} element={<Login />} />
           <Route path={ROUTES.SELECTED_PAGE} element={<h1></h1>} />
           <Route path={ROUTES.UNREGISTERED_PAGE} element={<h1></h1>} />
         </Routes>
--- src/constant/routes.ts
+++ src/constant/routes.ts
@@ -2,6 +2,5 @@ export const ROUTES = {
   SEARCH_PAGE: "/",
   VACANCY_PAGE: "/vacancy/:id",
   SELECTED_PAGE: "select",
-  LOGIN: "/login",
   UNREGISTERED_PAGE: "/*",
 };
