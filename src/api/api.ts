7643d37a855d1d9a58e60b2a8c4ca6be60c116b7
MaksimBozhko <bochkomax2000@gmail.com>
2023-04-26 02:15:08 +0300

    add: secter key

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,6 +3,7 @@ import axios from "axios"
 export const instance = axios.create({
   baseURL: "https://startup-summer-2023-proxy.onrender.com/2.0/",
   headers: {
-    "X-Api-App-Id": "v3.r.137440105.399b9c5f19384345afe0ad0339e619e71c66af1d.800f8642a38256679e908c370c44267f705c2909"
+    "X-Api-App-Id": "v3.r.137440105.399b9c5f19384345afe0ad0339e619e71c66af1d.800f8642a38256679e908c370c44267f705c2909",
+    "x-secret-key": "GEU4nvd3rej*jeh.eqp"
   }
 })
